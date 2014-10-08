ec618f8aa09d135e46f50c016e8b8df62c8c65a3
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-10-08 14:13:34 -0600

    build before start

--- index.js
+++ index.js
@@ -52,5 +52,7 @@ app.get('/api/jobs/:job/builds/:build/output', function(req, res){
 
 app.use(express.static(__dirname + '/dist'))
 
-app.listen(process.env.PORT || 3000)
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port %d', server.address().port)       
+})
 
