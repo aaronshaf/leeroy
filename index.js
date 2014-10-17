35f30ed59a64442f23943d748c28bd39ff55a309
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-10-17 13:02:27 -0600

    Poll every 15 seconds

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(server)
 var Job = require('./models/job')
 var Build = require('./models/build')
 var Promise = require('es6-promise').Promise
-setInterval(Build.updateRecent,30000)
+setInterval(Build.updateRecent,15000) // 15 seconds
 
 function partialListener(res) {
   return function(message) {
